a800eaae6123316fb2ef17c621d156a374134a89
Megan Chen <meganchen@utexas.edu>
2015-09-21 16:54:32 -0500

    changed theme and didn't actually add anything with git so recommitting

--- js/site.js
+++ js/site.js
@@ -1,8 +1,17 @@
 $(function() {
     loadAboutPage();
     
+    $('#name').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, '#about') ).offset().top
+    }, 500);
+    return false;
+});
+    
 });
 
 function loadAboutPage() {
     $('#about').load("/aboutme.html");  
 }
+
+
